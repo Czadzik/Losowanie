35c311bd1bc018ede87ab2633603bd5d4110da72
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-07-13 16:22:24 +0200

    Init MVVM

---
+++ LosowanieLotto/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using LosowanieLotto.ViewModels;
+
+namespace LosowanieLotto
+{
+    class Bootstrapper:BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+                Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
