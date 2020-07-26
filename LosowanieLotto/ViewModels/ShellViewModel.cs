009b20d20f921d71cad9578ee5218c6ff4b9cf99
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-07-26 16:53:21 +0200

    Binding changes

--- LosowanieLotto/ViewModels/ShellViewModel.cs
+++ LosowanieLotto/ViewModels/ShellViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Autofac;
 using Caliburn.Micro;
 
@@ -13,38 +14,128 @@ namespace LosowanieLotto.ViewModels
     {
         List<int> _listOfUserNumbers = new List<int>(6);
         List<int> _listOfDrawNumbers = new List<int>(6);
-        int[] numbers=new int[6];
 
-        private int _drawNumber = 2;
-
+        private int _drawNumber0;
+        private int _drawNumber1;
+        private int _drawNumber2;
+        private int _drawNumber3;
+        private int _drawNumber4;
+        private int _drawNumber5;
+        private bool _canDrawNumber=false;
+        private string _resultOfDraw;
         public int DrawNumber0
         {
-            get { return _drawNumber; }
+            get { return _drawNumber0; }
             set
             {
-                _drawNumber = value;
-                NotifyOfPropertyChange(() =>DrawNumber0 );
+                _drawNumber0 = value;
+                NotifyOfPropertyChange(() => DrawNumber0);
             }
         }
 
-        public bool CanStartDrawNumber(int drawNumber)
+        public int DrawNumber1
         {
-
-            return true;
+            get { return _drawNumber1; }
+            set
+            {
+                _drawNumber1 = value;
+                NotifyOfPropertyChange(() => DrawNumber1);
+            }
         }
-        public void startDrawNumber( )
+
+        public int DrawNumber2
+        {
+            get { return _drawNumber2; }
+            set
+            {
+                _drawNumber2 = value;
+                NotifyOfPropertyChange(() => DrawNumber2);
+            }
+        }
+
+        public int DrawNumber3
+        {
+            get { return _drawNumber3; }
+            set
+            {
+                _drawNumber3 = value;
+                NotifyOfPropertyChange(() => DrawNumber3);
+            }
+        }
+
+        public int DrawNumber4
+        {
+            get { return _drawNumber4; }
+            set
+            {
+                _drawNumber4 = value;
+                NotifyOfPropertyChange(() => DrawNumber4);
+            }
+        }
+
+        public int DrawNumber5
+        {
+            get { return _drawNumber5; }
+            set
+            {
+                _drawNumber5 = value;
+                NotifyOfPropertyChange(() => DrawNumber5);
+            }
+        }
+
+        public bool CanStartDrawNumber
+        {
+            get { return _canDrawNumber;}
+
+        }
+      
+        public string ResultOfDraw
+        {
+            get { return _resultOfDraw;}
+            set
+            {
+                _resultOfDraw = value;
+
+          
+                NotifyOfPropertyChange((() => ResultOfDraw));
+            }
+            //TODO AutoScroll 
+        }
+
+        public void StartDrawNumber(int drawNumber0)
         {
             var container = ContainerConifg.Configuer();
             using (var scope = container.BeginLifetimeScope())
             {
                 var app = scope.Resolve<IApplication>();
-               
-             _listOfUserNumbers= app.RunDrawNumberForUser();
-             numbers[0] = -_listOfUserNumbers[0];
-              MessageBox.Show(numbers[0].ToString());
-              _drawNumber = 99;
+
+                _listOfDrawNumbers = app.RunDrawNumberForUser();
             }
 
+            ResultOfDraw += _listOfDrawNumbers[0].ToString()+" "+ _listOfDrawNumbers[1].ToString()+ " " + _listOfDrawNumbers[2].ToString()+ " " +
+                            _listOfDrawNumbers[3].ToString()+ " " + _listOfDrawNumbers[4].ToString()+ " " + _listOfDrawNumbers[5].ToString()+"\n";
+            
+        }
+   
+        public void StartDrawUserNumber()
+        {
+            var container = ContainerConifg.Configuer();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+
+                _listOfUserNumbers = app.RunDrawNumberForUser();
+
+                DrawNumber0 = _listOfUserNumbers[0];
+                DrawNumber1 = _listOfUserNumbers[1];
+                DrawNumber2 = _listOfUserNumbers[2];
+                DrawNumber3 = _listOfUserNumbers[3];
+                DrawNumber4 = _listOfUserNumbers[4];
+                DrawNumber5 = _listOfUserNumbers[5];
+            }
+
+            _canDrawNumber = true;
+            NotifyOfPropertyChange(()=>CanStartDrawNumber);
         }
     }
 }
--- LosowanieLotto/Views/ShellView.xaml.cs
+++ LosowanieLotto/Views/ShellView.xaml.cs
@@ -22,6 +22,8 @@ namespace LosowanieLotto.Views
         public ShellView()
         {
             InitializeComponent();
+            
+           
         }
 
        
