fd34da37afdfd11aec7056b09ad7a70f3ca2255e
Czadzik <dns997@gmail.com>
2020-10-07 02:13:43 +0200

    Now check correct list

--- DrawLibrary/BusinessLogic.cs
+++ DrawLibrary/BusinessLogic.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using LosowanieLotto;
 
 namespace DrawLibrary
@@ -10,35 +8,35 @@ namespace DrawLibrary
         List<int> drawNumbersForUser(List<int> listOfUserNumbers);
 
         List<int> drawNumbersForWin(List<int> listOfUserNumbers);
-         bool[] CheckNumber(List<int> listOfUserNumbers, List<int> listOfNumbersForWin);
+        bool[] CheckNumber(List<int> listOfUserNumbers, List<int> listOfNumbersForWin);
     }
 
-  public  class BusinessLogic : IBuisnessLogic
+    public class BusinessLogic : IBuisnessLogic
     {
         private IDrawNumber _drawNumber;
 
 
         private bool[] howManyShoots;
+
         public BusinessLogic(IDrawNumber drawNumber)
         {
             _drawNumber = drawNumber;
         }
 
-        public List<int> drawNumbersForUser(List<int> listOfUserNumbers )
+        public List<int> drawNumbersForUser(List<int> listOfUserNumbers)
         {
             return _drawNumber.drawNumbers(listOfUserNumbers);
-         
         }
+
         public List<int> drawNumbersForWin(List<int> drawNumbersForUser)
         {
             return _drawNumber.drawNumbers(drawNumbersForUser);
-    
         }
-        public bool [] CheckNumber(List<int> listOfUserNumberss, List<int> listOfNumbersForWinn)
+
+        public bool[] CheckNumber(List<int> listOfUserNumberss, List<int> listOfNumbersForWinn)
         {
-            howManyShoots=_drawNumber.checkNumbers(listOfUserNumberss, listOfNumbersForWinn);
-           return howManyShoots;
+            howManyShoots = _drawNumber.checkNumbers(listOfNumbersForWinn, listOfUserNumberss);
+            return howManyShoots;
         }
-        
     }
 }
