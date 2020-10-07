b87c960ef468eb3278f4da596a5bcc39b807667f
Czadzik <dns997@gmail.com>
2020-10-07 02:13:54 +0200

    Now clone list

--- DrawLibrary/DrawNumber.cs
+++ DrawLibrary/DrawNumber.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace LosowanieLotto
 {
@@ -15,39 +13,54 @@ namespace LosowanieLotto
 
     public class DrawNumber : IDrawNumber
     {
-   
         public List<int> drawNumbers(List<int> ListOFNumbers)
         {
-            Random random= new Random();
+            Random random = new Random();
 
             for (int i = 0; i < ListOFNumbers.Capacity; i++)
             {
                 ListOFNumbers.Add(random.Next(1, 49));
-
             }
+
             return ListOFNumbers;
         }
-       
+
 
         public bool[] checkNumbers(List<int> UserListOFNumbers, List<int> DrawListOfNumbers)
         {
+            List<int> UsersNumbers = DeepClone(UserListOFNumbers);
             bool[] shoot = new[] {false, false, false, false, false, false};
-            for (int i = 0; i < UserListOFNumbers.Count; i++)
+            for (int i = 0; i < UsersNumbers.Count; i++)
             {
                 for (int j = 0; j < DrawListOfNumbers.Count; j++)
                 {
-                    if (UserListOFNumbers[i] == DrawListOfNumbers[j])
+                    // z jakiegoś powodu czasem występuje tu problem
+                    if (UsersNumbers[i] == DrawListOfNumbers[j])
                     {
                         shoot[i] = true;
-                        UserListOFNumbers.RemoveAt(i);
+                        UsersNumbers.RemoveAt(i);
                     }
-                }   
-                //TODO Poprawić zeby nie kasowało tablicy
+                    //////////////////////////////////////////////
+                }
+
+             
             }
+
             return shoot;
         }
-        
 
-
+    
+     
+        public static T DeepClone<T>(T obj)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                stream.Position = 0;
+ 
+                return (T) formatter.Deserialize(stream);
+            }
+        }
     }
 }
